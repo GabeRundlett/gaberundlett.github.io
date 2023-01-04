06a22591416a822141b4dbd6b9133a09b5a7be3a
nellfs <pokycelit@gmail.com>
2023-01-03 23:03:18 -0300

    feat: imgui window animation

--- src/components/ImguiWindow/index.tsx
+++ src/components/ImguiWindow/index.tsx
@@ -2,12 +2,14 @@ import './index.css';
 
 const ImguiWindow = () => {
   return (
-    <div className="imgui-window">
-      <div className="imgui-window__topbar">
-        <div className="triangle"></div>
-        <h1>Gabe Rundlett</h1>
+    <div className="imgui-background">
+      <div className="imgui-window">
+        <div className="imgui-window__topbar">
+          <div className="triangle"></div>
+          <h1>Gabe Rundlett</h1>
+        </div>
+        <div className="imgui-window__content"></div>
       </div>
-      <div className="imgui-window__content"></div>
     </div>
   );
 };
