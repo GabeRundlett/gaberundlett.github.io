136a7db8a7307f6ed214eb256642112de878a872
nellfs <pokycelit@gmail.com>
2023-01-07 23:54:25 -0300

    feat: home animations

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,33 +1,38 @@
 import { useState } from 'react';
-import { useScrollBlock } from '../../hooks/useScrollBlock';
 import './index.css';
 import Startup from '../../components/Startup';
+import { useInView } from 'react-intersection-observer';
 
 const Home = () => {
   const [showStartup, setShowStartup] = useState(true);
+  const { ref, inView } = useInView({
+    threshold: 0.1
+  });
 
   if (showStartup)
     return (
       <Startup
         onClick={() => {
           setShowStartup((oldState) => !oldState);
-          console.log('hello');
         }}
-      ></Startup>
+      />
     );
 
   return (
     <div className="home">
       <div className="hero-space">
-        <div className="banner">
-          <h2>
-            I am a young software engineer who loves <br /> making cool things
-            with code in my free-time
-          </h2>
+        <div className={`banner__${inView === true ? 'unfocused' : 'focused'}`}>
+          <div className="text">
+            <h1>Gabe Rundlett</h1>
+            <h2>
+              I am a young software engineer who loves <br /> making cool things
+              with code in my free-time
+            </h2>
+          </div>
         </div>
       </div>
-      <div className="container">
-        <div>hello</div>
+      <div className="container" ref={ref}>
+        <div>{`${inView}`}</div>
         <div>test</div>
         <div>hello</div>
         <div>test</div>
